67f2eded4c508cc3f20fc17353c8b739518f21ff
itsvarsharma <101050108+itsvarsharma@users.noreply.github.com>
2023-07-23 19:49:19 +0530

    added mongoose and userModel

---
+++ rhythm_backend/models/User.js
@@ -0,0 +1,41 @@
+const mongoose=require("mongoose");
+//create a mingoose
+//step1 require mongoose
+//step2 create a mongoose schema
+//step3 create a model
+const User=new mongoose.Schema({
+    firstName:{
+        type: String,
+        required:true,
+    },
+    last: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    likedSongs: {
+        
+        type: String, //we'll change it to array later
+        default: "",
+    },
+    likedPlaylists: {
+        type: String, //we'll change it to array later
+        default: "",
+    },
+    subscribedArtists: {
+        type: String, //we'll change it to array later
+        default: "",
+    },
+
+});
+
+const UserModel= mongoose.model("User",User);
+
+module.exports = UserModel;
